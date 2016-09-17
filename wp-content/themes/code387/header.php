359fb7ba28b0ba03857d31251e7c60246874f1a3
kekebeen <kakosi69@gmail.com>
2016-09-17 18:45:14 +0200

    Added terminal and hero banner

--- wp-content/themes/code387/header.php
+++ wp-content/themes/code387/header.php
@@ -21,10 +21,26 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'code387' ); ?></a>
 
-    <header id="masthead" class="site-header" role="banner">
-       
+    <header id="masthead" class="site-header bg-center cover pv6" role="banner">
+        <div class="terminal mw7 center ph3 ph2-ns tc br2 pv5 mb5">
+           <!-- <h1 class="fw6 f3  f2-ns lh-title mt0 mb3 white">This is a tagline. For x.</h1> -->
+           <div class="window shadow-1 br1 tl">
+               <div class="bar bg-black-80 br2 br--top ph3">
+                   <div class="btn relative br-100 db bg-light-red"></div>
+               </div>
+               <div class="body white bg-black-70 ph4 f6 f5-ns">
+                   <pre>
+                       <div class="comment"># Greetings Friend:</div>
+                       <div class="prompt"><span class="light-blue">info@code387.ba:~$ </span><span>You need custom CMS ?</span></div>
+                       <div class="prompt"><span class="light-blue">info@code387.ba:~$ </span><span>You need mobile app ?</span></div>
+                       <div class="prompt"><span class="light-blue">info@code387.ba:~$ </span><span>Let's work together!</span></div>
+                       <div class="prompt"><span class="light-blue">info@code387.ba:~$ </span><span>Scroll down for more</span></div>
+                       <div class="prompt"><span class="light-blue">info@code387.ba:~$ </span><span class="pulse">_</span></div>
+                   </pre>
+               </div>
+           </div>
+        </div><!-- terminal -->
     </header><!-- #masthead -->
 
     <div id="content" class="site-content">
