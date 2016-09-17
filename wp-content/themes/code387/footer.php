07baf9f818c4f705592f03c45a56300e60987c4a
kekebeen <kakosi69@gmail.com>
2016-09-17 17:28:15 +0200

    Added tracking to theme

---
+++ wp-content/themes/code387/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package code387
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'code387' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'code387' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'code387' ), 'code387', '<a href="http://www.benjaminbabic.com" rel="designer">Benjamin Babic</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/code387/gulpfile.js
@@ -0,0 +1,90 @@
+var batch = require('gulp-batch');
+var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var clearFix = require('postcss-clearfix');
+var colorShort = require('postcss-color-short');
+var cssMqpacker = require('css-mqpacker');
+var cssNano = require('cssnano');
+var discardComments = require('postcss-discard-comments');
+var focus = require('postcss-focus');
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var precss = require('precss');
+var atImport = require('postcss-import');
+var px2Rem = require('postcss-pxtorem');
+var short = require ('postcss-short');
+var cssnext = require('postcss-cssnext');
+var size = require('postcss-size');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+
+gulp.task('postcss', function () {
+  var processors = [
+    atImport,
+    precss,
+    colorShort,
+    focus,
+    cssnext({browsers: ['last 2 versions']}),
+    short,
+    size,
+    clearFix,
+    px2Rem,
+    cssMqpacker,
+    discardComments,
+    cssNano
+  ];
+
+  var reportError = function (error) {
+    notify({
+        title: 'Gulp Task Error',
+        message: 'Check the console.'
+    }).write(error);
+
+    console.log(error.toString());
+
+    this.emit('end');
+  }
+
+  return gulp.src('build/scss/style.css')
+    .pipe(plumber({
+          errorHandler: reportError
+      }))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream())
+    .on('error', reportError);
+});
+
+// JavaScript
+
+gulp.task('js', function () {
+  return gulp.src('build/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
+});
+
+var localDevUrl = 'local.dev/code387/';
+// Live reload sync on every screen connect to localhost
+gulp.task('init-live-reload', function() {
+  browserSync.init({
+    proxy: localDevUrl,
+    notify: false,
+    open:false
+  });
+});
+
+// Watch Files For Changes
+gulp.task('dev-watch', function() {
+  gulp.watch( './build/js/*.js', ['js'] );
+  gulp.watch( './build/scss/*.css', ['postcss']);
+  gulp.watch( './**/*.php').on('change', browserSync.reload);
+  gulp.watch( './style.css').on('change', browserSync.reload);
+});
+
+gulp.task('dev-watch-sync', ['init-live-reload', 'dev-watch']);
+
+// Default Task
+gulp.task('default', ['dev-watch-sync']);
