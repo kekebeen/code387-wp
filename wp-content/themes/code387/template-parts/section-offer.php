872855794cbfa20c1de39cf900efe67841025af5
kekebeen <kakosi69@gmail.com>
2016-09-19 02:33:51 +0200

    Added desktop diagonal section

--- wp-content/themes/code387/template-parts/section-offer.php
+++ wp-content/themes/code387/template-parts/section-offer.php
@@ -1,8 +1,32 @@
-<section class="mw8 center ph3-ns pv5 mv5">
-    <div class="separator-title mb4 tc">
-        <h1 class="ttu blue f2 fw5 mb2">What we offer</h1>
-        <?php get_template_part('template-parts/title-separator'); ?>
-    </div><!-- separator -->
-</section>
+<section class="mw-100 ph3-ns pv5 mv5">
+    <div class="diagonal">
+        <div class="info relative w100 bg-center cover z-1 white tc pv5 overflow-hidden">
+            <h1 class="ttu mb4 f3 mr7">what we offer</h1>
+            <div class="icon icon1 relative w-50 left-0 tr mb3">
+                <div class="icon__info dib">
+                    <b>RESPONSIVE</b>
+                    <div class="subtitle relative w-75 mt2">
+                        Responsive web applications using latest technology standards.
+                    </div>
+                </div>
+                <div class="icon__circle dib mb3 pa3 tc bg-white blue nowrap v-base br-100 relative lh-solid">
+                    <i class="ton-li-key f2"></i>
+                </div>
+            </div>
+            <div class="icon icon2 relative w-50 left-0 tr mb3">
+                <div class="icon__info dib">
+                    <b>MOBILE APPS</b>
+                    <div class="subtitle relative w-75 mt2">
+                        Beside web applications, we also offer mobile application development.
+                    </div>
+                </div>
+                <div class="icon__circle dib dib mb3 pa3 tc bg-white blue nowrap v-base br-100 relative lh-solid">
+                    <i class="ton-li-globe f2"></i>
+                </div>
+            </div>
+            <div class="colorinfo absolute bg-blue"></div>
+        </div><!-- info diagonal section -->
+    </div><!-- diagonal div -->
+</section><!-- offer section -->
 
 
