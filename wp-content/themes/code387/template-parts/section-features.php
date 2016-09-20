881000c10405be0e5bd7bf353cb657a00d0f2f72
kekebeen <kakosi69@gmail.com>
2016-09-20 15:14:21 +0200

    Added features hover animation

--- wp-content/themes/code387/template-parts/section-features.php
+++ wp-content/themes/code387/template-parts/section-features.php
@@ -5,43 +5,51 @@
     </div><!-- separator -->
 
     <div class="cf">
-        <div class="fl w-100 w-third-ns tc">
-            <div class="features-circle dib relative br-100 v-base nowrap lh-solid mb3 pa3 tc">
-                <i class="ton-li-key f1 blue"></i>
-            </div><!-- circle icon -->
-            <h2 class="ttu f3 fw5 mb2">web design</h2>
-            <div class="features-body pa3 tl">
-                <p>
-                    We start by analyzing your needs, and after that,
-                    start creating a pleasant design that match your company’s profile.
-                    We can also create a full graphic profile for you.
-                </p>
-            </div><!-- features body -->
-        </div>
-        <div class="fl w-100 w-third-ns tc">
-            <div class="features-circle dib relative br-100 v-base nowrap lh-solid mb3 pa3 tc">
-                <i class="ton-li-new-mail f1 blue"></i>
-            </div><!-- circle icon -->
-            <h2 class="ttu f3 fw5 mb2">web development</h2>
-            <div class="features-body pa3 tl">
-                <p>
-                    We use the design provided (or created by our own designer)
-                    and adds the nessasary funcitonallity to it, to suit your needs and ensure high level of conversions.
-                </p>
-            </div><!-- features body -->
-        </div>
-        <div class="fl w-100 w-third-ns tc">
-             <div class="features-circle dib relative br-100 v-base nowrap lh-solid mb3 pa3 tc">
-                <i class="ton-li-data-1 f1 blue"></i>
-            </div><!-- circle icon -->
-            <h2 class="ttu f3 fw5 mb2">seo</h2>
-            <div class="features-body pa3 tl">
-                <p>
-                    When your site is up and running you might need some help marketing your site,
-                    by SEO or by AdWords. We analyze what is best for you, and do the work, all within your set budget.
-                </p>
-            </div><!-- features body -->
-        </div>
+        <div class="fl w-100 w-third-ns tc pa2 feature__icon">
+            <div class="db bb bw2 b--blue w-100 h-100">
+                <div class="dib relative mb3 ">
+                    <i class="ton-li-key f1 blue"></i>
+                </div><!-- circle icon -->
+                <h2 class="ttu f3 fw5 mb2">web design</h2>
+                <div class="features-body pa3 tl">
+                    <p>
+                        We start by analyzing your needs, and after that,
+                        start creating a pleasant design that match your company’s profile.
+                        We can also create a full graphic profile for you.
+                    </p>
+                </div><!-- features body -->
+            </div><!-- border div -->
+        </div><!-- icon 1 -->
+
+        <div class="fl w-100 w-third-ns tc pa2 feature__icon">
+            <div class="db bb bw2 b--blue w-100 h-100">
+                <div class="dib relative mb3">
+                    <i class="ton-li-new-mail f1 blue"></i>
+                </div><!-- circle icon -->
+                <h2 class="ttu f3 fw5 mb2">web development</h2>
+                <div class="features-body pa3 tl">
+                    <p>
+                        We use the design provided (or created by our own designer)
+                        and adds the nessasary funcitonallity to it, to suit your needs and ensure high level of conversions.
+                    </p>
+                </div><!-- features body -->
+            </div><!-- border div -->
+        </div><!-- icon2-->
+
+        <div class="fl w-100 w-third-ns tc pa2 feature__icon">
+            <div class="db bb bw2 b--blue w-100 h-100">
+                <div class="dib relative mb3">
+                    <i class="ton-li-data-1 f1 blue"></i>
+                </div><!-- circle icon -->
+                <h2 class="ttu f3 fw5 mb2">seo</h2>
+                <div class="features-body pa3 tl">
+                    <p>
+                        When your site is up and running you might need some help marketing your site,
+                        by SEO or by AdWords. We analyze what is best for you, and do the work, all within your set budget.
+                    </p>
+                </div><!-- features body -->
+            </div><!-- border div -->
+        </div><!-- icon3 -->
     </div><!-- row for grid -->
-</section>
+</section><!-- features section -->
 
