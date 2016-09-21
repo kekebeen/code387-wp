9bc359692f44b8ca9e5065998524eb09ca80be49
kekebeen <kakosi69@gmail.com>
2016-09-21 16:43:23 +0200

    Made title separator mobile friendly/ changed padding

--- wp-content/themes/code387/template-parts/title-separator.php
+++ wp-content/themes/code387/template-parts/title-separator.php
@@ -1,3 +1,3 @@
-<div class="or-divider flex flex-row justify-center items-center pv0 ph7 mv0 mh1">
+<div class="or-divider flex flex-row justify-center items-center pv0 ph4 ph7-ns mv0 mh1">
     <i class="ton-li-heart blue f4"></i>
 </div>
